f16bb2084f501f8506f826f1774b6b99632fdf5e
okborges <rafaelborges.av@gmail.com>
2022-11-12 22:17:39 -0300

    hotfix tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
